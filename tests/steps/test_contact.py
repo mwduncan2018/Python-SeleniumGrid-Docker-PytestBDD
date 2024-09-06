c98ce857ce84492ded76341b1da34ee58899590a
Michael Duncan <mwduncan2018@gmail.com>
2024-09-06 10:40:34 -0400

    Update contact steps

--- tests/steps/test_contact.py
+++ tests/steps/test_contact.py
@@ -9,27 +9,19 @@ scenarios('../features/contact.feature')
 def _(scenario_context: dict):
     page: Page = scenario_context["page"]
     page.navbar.go_to_contact()
-    import time
-    time.sleep(1)
 
 
 @then(parsers.parse("the footer text should include {footerText} within {seconds:d} seconds"))
 def _(scenario_context: dict, footerText: str, seconds: int):
     page: Page = scenario_context["page"]
     assert page.contact.does_footer_text_contain(footerText, seconds)
-    import time
-    time.sleep(1)
 
 @then(parsers.parse("the GitHub link {gitHub} should be provided"))
 def _(scenario_context: dict, gitHub: str):
     page: Page = scenario_context["page"]
     assert page.contact.get_github_href() == gitHub
-    import time
-    time.sleep(1)
 
 @then(parsers.parse("the following {skill} should be listed"))
 def _(scenario_context: dict, skill: str):
     page: Page = scenario_context["page"]
     assert skill in page.contact.get_list_of_skills()
-    import time
-    time.sleep(1)
